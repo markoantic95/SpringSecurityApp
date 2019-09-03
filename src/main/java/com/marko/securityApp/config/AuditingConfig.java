cc0c2c0ef33fa722fee40f3123e5b3cb26eec65b
Marko Antic <markoantic95@gmail.com>
2019-09-03 15:48:19 +0200

    Added UserDateAudit class - used for user auditing. Modified auditing configuration class. It returns id of current user.

--- src/main/java/com/marko/securityApp/config/AuditingConfig.java
+++ src/main/java/com/marko/securityApp/config/AuditingConfig.java
@@ -5,18 +5,43 @@
  */
 package com.marko.securityApp.config;
 
+import com.marko.securityApp.security.UserPrincipal;
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  *
  * @author Marko
  */
-// implementation of AuditorAware interface - it doesnt return name(which is default behavior valjda) but id of principal
 @Configuration
 @EnableJpaAuditing  // we want to enable auditing via annotation configuration
 public class AuditingConfig {
-    // In order to configure the app to use AuditorAwareImpl to look up the current principal, 
- //   declare a bean of AuditorAware type initialized with an instance of AuditorAwareImpl and 
-    //specify the bean's name as the auditorAwareRef parameter's value in @EnableJpaAuditing:
+
+    @Bean
+    public AuditorAware<Long> auditorProvider() {
+        return new SpringSecurityAuditAwareImpl();
+    }
+}
+//In case you use either @CreatedBy or @LastModifiedBy, the auditing infrastructure somehow needs to become aware of the current principal.
+//To do so, we provide an AuditorAware<T> SPI interface that you have to implement to tell the infrastructure who the current user or system 
+//interacting with the application is. The generic type T defines of what type the properties annotated with @CreatedBy or @LastModifiedBy have to be.
+
+class SpringSecurityAuditAwareImpl implements AuditorAware<Long> {
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+        return Optional.ofNullable(principal.getId());
+    }
+
 }
